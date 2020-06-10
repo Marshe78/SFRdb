a8b31f4cb1870bc0a788790e768f932a68e11a30
R.Quentin <59691299+Marshe78@users.noreply.github.com>
2020-06-10 14:02:39 +0200

    Add files via upload

---
+++ Table.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS SFRdb;
+
+
+
+DROP TABLE IF EXISTS `user`; 
+CREATE TABLE `user`( 
+ `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+ `civilite` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL, 
+ `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL, 
+ `prenom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `listeconnu` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `commentaire` varchar(1800) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `copieCoche` varchar(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL )
+ ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
